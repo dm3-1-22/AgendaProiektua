0b1c105fa7255ab7ea1b76a37eed48229f23f1ac
Lucia <dm3-1-22.17@uni.eus>
2018-03-16 12:27:23 +0100

    eventos klasea

--- Agenda/Agenda/Erabiltzailea.py
+++ Agenda/Agenda/Erabiltzailea.py
@@ -8,3 +8,16 @@ class Erabiltzailea:
     
 erabil1 = Erabiltzailea ('Lucia', 'Berriz', 9876, 1)
 print('NAN:', erabil1.b1,',Izena:',erabil1.b2,',Helbidea:',erabil1.b3,',Telefonoa:',erabil1.b4)
+
+class Eventos:
+    
+    def __init__(self, izena, kodea, inportantzia, lekua, hasiera_data, bukaera_data:        
+        self.i1=izena
+        self.i2=kodea
+        self.i3=inportantzia
+        self.i4=lekua
+        self.i5=hasiera_data
+        self.i6=bukaera_data     
+    
+eventos1 = Eventos ('Lucia', 9876, true,  1)
+print('NAN:', erabil1.i1,',Izena:',erabil1.i2,',Helbidea:',erabil1.i3,',Telefonoa:',erabil1.i4)
